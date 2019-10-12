58e5423697be448051713aa912ea8d1f85ca2efb
Rae Hewitt <rae.hewitt@outlook.com>
2019-10-12 17:12:13 +0100

    Add project files.

---
+++ Form1.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace Nano_Jukebox
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            DirectoryInfo dir = new DirectoryInfo(Environment.GetFolderPath(Environment.SpecialFolder.MyMusic));
+            FileInfo[] songs = dir.GetFiles("*.mp3,*.mp4");
+            foreach(FileInfo song in songs)
+            {
+                Panel NewPanel = new Panel_LibrarySong();
+            }
+        }
+    }
+}
