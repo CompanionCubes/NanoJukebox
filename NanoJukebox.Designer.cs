2afce84b48dadc992aab12971ebeec2f036b6815
ge0 <ge0@mail.com>
2019-10-12 20:09:07 +0100

    Fonts

--- NanoJukebox.Designer.cs
+++ NanoJukebox.Designer.cs
@@ -62,11 +62,11 @@
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Font = new System.Drawing.Font("Nunito", 26.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 26.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label1.ForeColor = System.Drawing.Color.White;
             this.label1.Location = new System.Drawing.Point(12, 18);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(129, 48);
+            this.label1.Size = new System.Drawing.Size(124, 39);
             this.label1.TabIndex = 0;
             this.label1.Text = "Library";
             // 
@@ -103,11 +103,11 @@
             this.label4.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
             this.label4.AutoSize = true;
-            this.label4.Font = new System.Drawing.Font("Nunito Light", 9.749999F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.749999F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label4.ForeColor = System.Drawing.Color.White;
-            this.label4.Location = new System.Drawing.Point(8, 62);
+            this.label4.Location = new System.Drawing.Point(8, 64);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(64, 18);
+            this.label4.Size = new System.Drawing.Size(64, 16);
             this.label4.TabIndex = 4;
             this.label4.Text = "Play With";
             // 
@@ -129,11 +129,11 @@
             this.Label_LibrarySongArtist.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
             this.Label_LibrarySongArtist.AutoSize = true;
-            this.Label_LibrarySongArtist.Font = new System.Drawing.Font("Nunito Light", 9.749999F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Label_LibrarySongArtist.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.749999F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.Label_LibrarySongArtist.ForeColor = System.Drawing.Color.White;
             this.Label_LibrarySongArtist.Location = new System.Drawing.Point(59, 25);
             this.Label_LibrarySongArtist.Name = "Label_LibrarySongArtist";
-            this.Label_LibrarySongArtist.Size = new System.Drawing.Size(71, 18);
+            this.Label_LibrarySongArtist.Size = new System.Drawing.Size(72, 16);
             this.Label_LibrarySongArtist.TabIndex = 2;
             this.Label_LibrarySongArtist.Text = "Song Artist";
             // 
@@ -142,11 +142,11 @@
             this.Label_LibrarySongTitle.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
             this.Label_LibrarySongTitle.AutoSize = true;
-            this.Label_LibrarySongTitle.Font = new System.Drawing.Font("Nunito", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Label_LibrarySongTitle.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.Label_LibrarySongTitle.ForeColor = System.Drawing.Color.White;
             this.Label_LibrarySongTitle.Location = new System.Drawing.Point(60, 4);
             this.Label_LibrarySongTitle.Name = "Label_LibrarySongTitle";
-            this.Label_LibrarySongTitle.Size = new System.Drawing.Size(82, 21);
+            this.Label_LibrarySongTitle.Size = new System.Drawing.Size(80, 20);
             this.Label_LibrarySongTitle.TabIndex = 1;
             this.Label_LibrarySongTitle.Text = "Song Title";
             // 
@@ -162,11 +162,11 @@
             // 
             this.Label_LibraryPage.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
             this.Label_LibraryPage.AutoSize = true;
-            this.Label_LibraryPage.Font = new System.Drawing.Font("Nunito Light", 9.749999F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Label_LibraryPage.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.749999F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.Label_LibraryPage.ForeColor = System.Drawing.Color.White;
             this.Label_LibraryPage.Location = new System.Drawing.Point(75, 265);
             this.Label_LibraryPage.Name = "Label_LibraryPage";
-            this.Label_LibraryPage.Size = new System.Drawing.Size(49, 18);
+            this.Label_LibraryPage.Size = new System.Drawing.Size(51, 16);
             this.Label_LibraryPage.TabIndex = 2;
             this.Label_LibraryPage.Text = "Page 1";
             // 
@@ -197,11 +197,11 @@
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Font = new System.Drawing.Font("Nunito", 26.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 26.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label2.ForeColor = System.Drawing.Color.White;
             this.label2.Location = new System.Drawing.Point(12, 378);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(220, 48);
+            this.label2.Size = new System.Drawing.Size(209, 39);
             this.label2.TabIndex = 4;
             this.label2.Text = "Now Playing";
             // 
@@ -229,11 +229,11 @@
             // 
             this.Label_NowPlayingArtist.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right)));
             this.Label_NowPlayingArtist.AutoSize = true;
-            this.Label_NowPlayingArtist.Font = new System.Drawing.Font("Nunito Light", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Label_NowPlayingArtist.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.Label_NowPlayingArtist.ForeColor = System.Drawing.Color.White;
             this.Label_NowPlayingArtist.Location = new System.Drawing.Point(165, 51);
             this.Label_NowPlayingArtist.Name = "Label_NowPlayingArtist";
-            this.Label_NowPlayingArtist.Size = new System.Drawing.Size(150, 36);
+            this.Label_NowPlayingArtist.Size = new System.Drawing.Size(147, 31);
             this.Label_NowPlayingArtist.TabIndex = 7;
             this.Label_NowPlayingArtist.Text = "Song Artist";
             // 
@@ -241,11 +241,11 @@
             // 
             this.Label_NowPlayingTitle.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right)));
             this.Label_NowPlayingTitle.AutoSize = true;
-            this.Label_NowPlayingTitle.Font = new System.Drawing.Font("Nunito", 26.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Label_NowPlayingTitle.Font = new System.Drawing.Font("Microsoft Sans Serif", 26.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.Label_NowPlayingTitle.ForeColor = System.Drawing.Color.White;
             this.Label_NowPlayingTitle.Location = new System.Drawing.Point(163, 3);
             this.Label_NowPlayingTitle.Name = "Label_NowPlayingTitle";
-            this.Label_NowPlayingTitle.Size = new System.Drawing.Size(178, 48);
+            this.Label_NowPlayingTitle.Size = new System.Drawing.Size(172, 39);
             this.Label_NowPlayingTitle.TabIndex = 6;
             this.Label_NowPlayingTitle.Text = "Song Title";
             // 
@@ -263,11 +263,11 @@
             // 
             this.label3.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
             this.label3.AutoSize = true;
-            this.label3.Font = new System.Drawing.Font("Nunito", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label3.ForeColor = System.Drawing.Color.White;
             this.label3.Location = new System.Drawing.Point(614, 674);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(115, 26);
+            this.label3.Size = new System.Drawing.Size(111, 24);
             this.label3.TabIndex = 9;
             this.label3.Text = "Powered by";
             // 
--- NanoJukebox.cs
+++ NanoJukebox.cs
@@ -8,6 +8,8 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 using System.IO;
+using System.Drawing.Text;
+using System.Runtime.InteropServices;
 
 namespace Nano_Jukebox
 {
@@ -16,19 +18,43 @@ namespace Nano_Jukebox
         public NanoJukebox()
         {
             InitializeComponent();
+            InitializeFonts();
             GetSongs();
+        }
+
+        public void InitializeFonts()
+        {
+            PrivateFontCollection pfc = new PrivateFontCollection();
+            int fontLength;
+            byte[] fontdata;
+            System.IntPtr data;
+
+            //add nunito light to the internal fonts
+            fontLength = Properties.Resources.Nunito_Light.Length;
+            fontdata = Properties.Resources.Nunito_Light;
+            data = Marshal.AllocCoTaskMem(fontLength);
+            Marshal.Copy(fontdata, 0, data, fontLength);
+            pfc.AddMemoryFont(data, fontLength);
+            
+            //add nunito regular to the internal fonts
+            fontLength = Properties.Resources.Nunito_Regular.Length;
+            fontdata = Properties.Resources.Nunito_Regular;
+            data = Marshal.AllocCoTaskMem(fontLength);
+            Marshal.Copy(fontdata, 0, data, fontLength);
+            pfc.AddMemoryFont(data, fontLength);
         }
 
         public void GetSongs()
         {
             DirectoryInfo dir = new DirectoryInfo(Environment.GetFolderPath(Environment.SpecialFolder.MyMusic));
-            FileInfo[] songs = dir.GetFiles("*.m4a");
+            String[] extensions =new[] {".m4a",".mp3" };
+            FileInfo[] songs = dir.EnumerateFiles().Where(f => )
 
             int x = 0;
             int y = 0;
             foreach (FileInfo song in songs)
             {
-                //Console.WriteLine(song.ToString());
+                Console.WriteLine(song.ToString());
 
                 Panel NewPanel = new Panel();
                 //NewPanel.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
